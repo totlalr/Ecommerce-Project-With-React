94fa370cd3071baa69b23cd46f5abb429d6c476f
Armin Vaghef <armin88vaghef@gmail.com>
2022-12-18 13:36:48 +0330

    Adding the possibility of increasing or decreasing the purchase of products, as well as component changes, searching and connecting it to state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
-
 import styles from "./Card.module.css";
+import down from "../images/down.svg";
+import up from "../images/up.svg";
 
 class Card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  downHandler = () => {
+    if (this.state.counter >= 1) {
+      this.setState((prevState) => ({
+        counter: prevState.counter - 1,
+      }));
+    }
+  };
+
+  upHandler = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
   render() {
     const { image, name, cost } = this.props;
+    const { counter } = this.state;
     return (
       <div className={styles.container}>
         <img alt="smart phone" src={image} />
         <h3>{name}</h3>
         <p> {cost}</p>
+
+        <div className={styles.counter}>
+          <img
+            className={this.state.counter ? "" : styles.deactive}
+            src={down}
+            alt="arrow"
+            onClick={this.downHandler}
+          />
+          <span>{counter}</span>
+          <img src={up} alt="arrow" onClick={this.upHandler} />
+        </div>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,32 @@ import React, { Component } from "react";
 import styles from "./Search.module.css";
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+    };
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
   render() {
     return (
       <div className={styles.container}>
         <p>Search what You Want</p>
-        <input placeholder="Search ..." />
+        <div>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.changeHandler}
+            placeholder="Search ..."
+          />
+          <br />
+          <span>{this.state.text}</span>
+        </div>
       </div>
     );
   }

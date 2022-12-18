b317f94389c50c14e94ee3a90dbe8d52d104df77
Armin Vaghef <armin88vaghef@gmail.com>
2022-12-18 14:21:32 +0330

    Optimize codes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,13 +7,30 @@ import iphone12 from "../images/iphone12.jpg";
 import s21 from "../images/s21.jpg";
 
 class Cards extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      phoneData: [
+        { id: 1, image: iphonex, name: "Iphone X", cost: "500 $" },
+        { id: 2, image: iphone11, name: "Iphone 11", cost: "600 $" },
+        { id: 3, image: iphone12, name: "Iphone 12", cost: "800 $" },
+        { id: 4, image: s21, name: "S21 Ultra", cost: "790 $" },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className={styles.container}>
-        <Card image={iphonex} name="Iphone X" cost="500 $" />
-        <Card image={iphone11} name="Iphone 11" cost="600 $" />
-        <Card image={iphone12} name="Iphone 12" cost="800 $" />
-        <Card image={s21} name="S21 Ultra" cost="790 $" />
+        {this.state.phoneData.map((phone) => (
+          <Card
+            image={phone.image}
+            name={phone.name}
+            cost={phone.cost}
+            key={phone.id}
+          />
+        ))}
       </div>
     );
   }
